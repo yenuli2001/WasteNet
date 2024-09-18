d39ded2ea4d374612a62157a2e33982d0ab71fa9
IT21304910 <yenuli.sirisena@yahoo.com>
2024-09-18 13:37:56 +0530

    commit changes in layout

--- app/(tab)/_layout.jsx
+++ app/(tab)/_layout.jsx
@@ -8,18 +8,12 @@ export default function TabLayout() {
   return (
     <Tabs screenOptions={{
       headerShown: false,
-      tabBarActiveTintColor:Colors.DGREEN
+      tabBarActiveTintColor:Colors.BTN
     }}>
-      <Tabs.Screen  name='home'
-         options={{
+       <Tabs.Screen  name='home'
+        options={{
           tabBarLabel:'Home',
           tabBarIcon:({color})=><Ionicons name="home" size={24} color={color}/>
-         }}
-      />
-      <Tabs.Screen  name='profile'
-        options={{
-          tabBarLabel:'Profile',
-          tabBarIcon:({color})=><Ionicons name="people-circle" size={24} color={color}/>
          }}
       />
     </Tabs>
