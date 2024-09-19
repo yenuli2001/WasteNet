086a10f520e6063bd3500c533cb9fbf7af6d7b78
IT21304910 <yenuli.sirisena@yahoo.com>
2024-09-19 17:37:31 +0530

    Update MenuList.jsx

--- components/Home/MenuList.jsx
+++ components/Home/MenuList.jsx
@@ -93,8 +93,8 @@ export default function MenuList() {
         }}>
            <Image source={item.icon}
            style={{
-             width: 40,
-             height: 40
+             width: 45,
+             height: 45
            }}
            />
            <Text style={{
